5f4f4feb126a7a41d683ff3ccd6044d9729561d7
Nettal <n2lf@outlook.com>
2022-01-11 20:23:16 +0800

    修复配置文件无效的BUG

--- src/main/java/n2lf.secureexplosion/SecureExplosion.java
+++ src/main/java/n2lf.secureexplosion/SecureExplosion.java
@@ -1,5 +1,6 @@
 package n2lf.secureexplosion;
 
+import net.minecraft.world.Explosion;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -12,16 +13,18 @@ import org.apache.logging.log4j.LogManager;
 public class SecureExplosion {
     public static final String MODID = "secureexplosion";
 
-    public SecureExplosion(){
-        LogManager.getLogger().info("Author:n2lf");
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON , Config.COMMON_CONFIG);
+    public SecureExplosion() {
+        //       LogManager.getLogger().info("Author:n2lf");
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
     }
 
     @SubscribeEvent
     public static void onExplosionStart(ExplosionEvent event) {
-    //    System.out.println("getExploder:"+event.getExplosion().getExploder()+":getSourceMob:"+event.getExplosion().getSourceMob());
+        Explosion explosion = event.getExplosion();
+        //System.out.println("getExploder:"+event.getExplosion().getExploder()+":getSourceMob:"+event.getExplosion().getSourceMob());
         if (Config.InterceptALL.get() ||
-                Config.InterceptExploder.get() || Config.InterceptSourceMob.get()) {
+                (Config.InterceptExploder.get() && explosion.getExploder() != null) ||
+                (Config.InterceptSourceMob.get() && explosion.getSourceMob() != null)) {
             event.getExplosion().clearToBlow();
         }
     }
