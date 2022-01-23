19ceaebe25015415a3e983481476938344321b31
Nettal <n2lf@outlook.com>
2022-01-23 13:16:03 +0800

    添加白名单功能

--- src/main/java/n2lf.secureexplosion/Config.java
+++ src/main/java/n2lf.secureexplosion/Config.java
@@ -2,11 +2,18 @@ package n2lf.secureexplosion;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Config {
     public static ForgeConfigSpec COMMON_CONFIG;
     public static ForgeConfigSpec.BooleanValue InterceptALL;
     public static ForgeConfigSpec.BooleanValue InterceptExploder;
     public static ForgeConfigSpec.BooleanValue InterceptSourceMob;
+    public static ForgeConfigSpec.BooleanValue PrintExploderInfo;
+    public static ForgeConfigSpec.BooleanValue EnableWhiteList;
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> WhiteList;
 
     static {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
@@ -17,6 +24,23 @@ public class Config {
                 .define("InterceptExploder",false);
         InterceptSourceMob = builder.comment("If the SourceMob can be found, intercept the explosion")
                 .define("InterceptSourceMob",true);
+        PrintExploderInfo = builder.comment("Print the Exploder info and CanonicalName")
+                .define("PrintExploder",true);
+        EnableWhiteList = builder.comment("Enable the WhiteList")
+                .define("EnableWhiteList",true);
+        WhiteList = builder.comment("Add the CanonicalName of Exploder's class, the added classes will not intercept\n"+
+                        "Example:WhiteList = [\"mekanism.additions.common.entity.EntityObsidianTNT\"," +
+                        " \"mekanism.additions.common.entity.baby.EntityBabyCreeper\" ," +
+                        "\"net.minecraft.entity.monster.CreeperEntity\"," +
+                        "\"net.minecraft.entity.item.TNTEntity\"]")
+                .defineList("WhiteList", Arrays.asList("", ""), new Predicate<Object>() {
+                    @Override
+                    public boolean test(Object o) {
+                      //  System.out.println(o);
+                        return true;
+                    }
+                });
+
         COMMON_CONFIG = builder.build();
     }
 }
--- src/main/java/n2lf.secureexplosion/SecureExplosion.java
+++ src/main/java/n2lf.secureexplosion/SecureExplosion.java
@@ -1,5 +1,6 @@
 package n2lf.secureexplosion;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.world.Explosion;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -7,23 +8,37 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(SecureExplosion.MODID)
 @Mod.EventBusSubscriber()
 public class SecureExplosion {
     public static final String MODID = "secureexplosion";
+    public static Logger LOGGER = LogManager.getLogger();
 
     public SecureExplosion() {
-        //       LogManager.getLogger().info("Author:n2lf");
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
     }
 
     @SubscribeEvent
     public static void onExplosionStart(ExplosionEvent event) {
+        if (!(event instanceof ExplosionEvent.Detonate))
+            return;
         Explosion explosion = event.getExplosion();
-        //System.out.println("getExploder:"+event.getExplosion().getExploder()+":getSourceMob:"+event.getExplosion().getSourceMob());
+        Entity exploder = explosion.getExploder();
+        if (Config.PrintExploderInfo.get()) {
+            if (exploder == null) {
+                LOGGER.info("[SecureExplosion]:Can not get Exploder,Exploder is null");
+            } else {
+                LOGGER.info(String.format("[SecureExplosion]:ExploderInfo:%s;CanonicalName:%s%n",exploder , exploder.getClass().getCanonicalName()));
+            }
+        }
+        if(exploder != null && Config.EnableWhiteList.get()
+                && Config.WhiteList.get().contains(exploder.getClass().getCanonicalName())){
+            return;
+        }
         if (Config.InterceptALL.get() ||
-                (Config.InterceptExploder.get() && explosion.getExploder() != null) ||
+                (Config.InterceptExploder.get() && exploder != null) ||
                 (Config.InterceptSourceMob.get() && explosion.getSourceMob() != null)) {
             event.getExplosion().clearToBlow();
         }
