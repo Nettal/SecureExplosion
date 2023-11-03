7d2f064623733ba9100a389fd0c8bc80df7507b4
SnowNF <SnowNF@outlook.com>
2023-11-03 23:49:10 +0800

    bump supported minecraft version to 1.19.2

--- src/main/java/nettal/secureexplosion/SecureExplosion.java
+++ src/main/java/nettal/secureexplosion/SecureExplosion.java
@@ -2,7 +2,7 @@ package nettal.secureexplosion;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Explosion;
-import net.minecraftforge.event.world.ExplosionEvent;
+import net.minecraftforge.event.level.ExplosionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
